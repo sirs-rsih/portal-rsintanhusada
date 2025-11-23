5d387a3ef9b2d3328d558c28283821c27bac1053
SIRS RS Intan Husada <almoch9@gmail.com>
2025-11-23 20:11:08 +0700

    Penambahan fungsi upload_file ke SPO, PANDUAN, PEDOMAN dan FORMULIR

--- pages/controller/vspo.php
+++ pages/controller/vspo.php
@@ -1,5 +1,6 @@
 <?php
   include('../../config.php');
+  include('../helpers/upload_helper.php');
 
   $unit       = $_GET['unit'];
   $judul      = $_POST['judul'];
@@ -21,8 +22,8 @@
     return false;
   }else if(in_array($ekstensi, $ekstensi_diperbolehkan) === true){
     // if($ukuran < 5242880){
-      $lokasi = '../../files/'.$unit.'-'.$judul.'.'.$ekstensi;
-      move_uploaded_file($file_tmp,$lokasi);
+      $hasil = uploadFile($ekstensi, $unit, $judul, $file_tmp);
+      $lokasi = $hasil['path'];
       $query = mysqli_query($connect,"INSERT INTO spo VALUES(NULL,'$unit','$judul','$deskripsi','$lokasi',now())");
       if($query){
         header('location:../../pages/spo/index.php?unit='.$unit.'&message=success');
